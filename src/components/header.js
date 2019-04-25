869c5081d715722a3035310fa80860f7ddeb52c9
Patrick O'Connell <poconnell@Patricks-MacBook-Pro.local>
2019-04-24 20:07:26 -0700

    Added theme variables / Moved tagline to index

--- src/components/header.js
+++ src/components/header.js
@@ -8,40 +8,26 @@ import logo from 'images/logotype.svg'
 
 const HeadWrapper = styled.header `
     width: 100%;
+    padding: 0 50px;
 `
-
 const Logo = styled.img `
     display: block;
 `
-const LogoLnk = styled(Link)
-`
+const LogoLnk = styled(Link)`
     display: block;
     padding-top: 48px;
-    padding-left: 50px;
 `
 const Menu = styled.nav `
     float: right;
-    margin-right: 50px;
     margin-top: 20px;
 `
-const MenuLnk = styled(Link)
-`
+const MenuLnk = styled(Link)`
     display: inline-block;
-    font-size: 24px;
+    font-size: ${props => props.theme.txtsm};
     font-decoration: none;
     font-weight: 200;
     color: #1E3783;
     padding-left: 44px;
-`
-const TaglineWrapper = styled.div `
-        display: flex;
-        justify-content: center;
-        margin: 70px 0;
-`
-const Tagline = styled.div `
-    font-size: 41px;
-    font-weight: 200;
-    width: calc(100% / 3 * 2);
 `
 
 const Header = ({siteTitle}) => (
@@ -54,11 +40,6 @@ const Header = ({siteTitle}) => (
             < Logo src = {logo}
               alt = {siteTitle} />
         </LogoLnk>
-        <TaglineWrapper>
-            <Tagline>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce viverra ipsum sed sapien dictum, ut tincidunt nisi.
-            </Tagline>
-        </TaglineWrapper>
     </HeadWrapper>
 )
 
