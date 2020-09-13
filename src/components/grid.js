a1ea7b3cdd94d2cffe68168d71605899a8e39375
Patrick O'Connell <poconnell@Patricks-MacBook-Pro.local>
2020-09-13 13:30:50 -0700

    Reconfigured and styled homepage archive secdtion

--- src/components/grid.js
+++ src/components/grid.js
@@ -5,66 +5,93 @@ import styled from 'react-emotion'
 const GridWrapper = styled.div`
   display: flex;
   flex-wrap: wrap;
-  background: linear-gradient(#BCDADA, #98CC84);
-  padding: 86px 25px 0;
+  justify-content: space-between;
+  border-top: offset 15px #e5e5e5;
+  background: linear-gradient(#bcdada 45%, #98cc84);
+  padding: 114px 10.41% 0;
+  @media (max-width: 420px) {
+      padding-top: 36px;
+  }
 `
 const GridTitle = styled.h4`
-    text-decoration: none;
-    font-size: ${props => props.theme.txtmd};
-    font-weight: 200;
-    font-style: italic;
-    margin-top: 1rem;
-    margin-left: 0.5rem;
+  text-decoration: none;
+  font-size: 1.42rem;
+  font-weight: 600;
 `
 
-const GridHero = styled.picture`
-  width: 100%;
+const GridHero = styled.div`
+  height: 450px;
+  margin: 0;
+  @media (max-width: 420px) {
+      height: auto;
+  }
   img {
-    border: 2px ${props => props.theme.bicblue} solid;
+    border: ${props => props.theme.border};
     width: 100%;
+    height: 450px;
+    object-fit: cover;
+    @media (max-width: 420px) {
+        border: ${props => props.theme.borderMobile};
+        height: 45vh;
+    }
   }
 `
 const Article = styled.article`
-    padding: 0 25px 92px;
-    width: calc(100%/3);
-    a {
-        text-decoration: none;
-        color: ${props => props.theme.bicblue};
-        :hover {
-            text-decoration: underline;
-        }
+  width: calc(33.33% - 16px);
+  a {
+    text-decoration: none;
+    :hover {
+      text-decoration: underline;
     }
+  }
+  @media (max-width: 420px) {
+      width: 100%;
+      margin-bottom: 36px;
+  }
+`
+
+const GridWorkType = styled.div`
+  text-decoration: none;
+  font-weight: 400;
+`
+
+const GridInfo = styled.div`
+  padding: 0.3rem 0.25rem;
+  color: #454545;
+  line-height: 140%;
 `
 
 const Grid = () => (
   <StaticQuery
     query={graphql`
-        query GridQuery {
-    allProject(skip: 2, limit: 6, sort: {fields: [sort], order: ASC}) {
-      edges {
-        node {
-          heroImage {
-            url
-            altText
+      query GridQuery {
+        allProject(skip: 3, limit: 3, sort: { fields: [sort], order: ASC }) {
+          edges {
+            node {
+              heroImage {
+                url
+                altText
+              }
+              title
+              workType
+              slug
+            }
           }
-          title
-          slug
         }
       }
-    }
-  }
     `}
     render={data => (
       <GridWrapper>
         {data.allProject.edges.map(({ node }, index) => (
-          <Article
-            key=
-            {index}>
+          <Article key={index}>
             <Link to={'/' + node.slug}>
               <GridHero>
                 <img src={node.heroImage.url} alt={node.altText} />
               </GridHero>
-              <GridTitle>{node.title}</GridTitle>
+              <GridInfo>
+                <GridTitle>{node.title}</GridTitle>
+                <GridWorkType>{node.workType}</GridWorkType>
+              </GridInfo>
             </Link>
           </Article>
         ))}
