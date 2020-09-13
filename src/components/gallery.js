f658face4acf12117673df8cb5fc33f2c7ee253c
Patrick O'Connell <poconnell@Patricks-MacBook-Pro.local>
2020-09-13 13:23:48 -0700

    Applied new styles and restructured project

---
+++ src/components/gallery.js
@@ -0,0 +1,55 @@
+import React from 'react'
+import { Link } from 'gatsby'
+import styled from 'react-emotion'
+
+const Wrapper = styled.section`
+  display: flex;
+  width: 100%;
+  justify-content: center;
+`
+
+const GalleryWrap = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  margin: 23px 23px 0;
+   @media (max-width: 420px) {
+       margin: 23px 0 0;
+   }
+  div {
+    width: calc(100% / 6);
+    padding: 30px;
+    border: 2px solid #e5e5e5;
+    border-radius: 8px;
+     @media (max-width: 420px) {
+         padding: 0;
+         border: none;
+         border-radius: 0;
+         width: calc(100% / 3)
+     }
+  }
+  img {
+    width: 100%;
+  }
+`
+
+
+export default function Gallery(props) {
+if(props.galleryImage.length === 0){
+    return null;
+}
+
+  return (
+<Wrapper>
+  <GalleryWrap>
+    <div>
+        <img src={props.heroThumb} />
+    </div>
+    {props.galleryImage.length}
+    {props.galleryImage.map(({ url, altText }, index) => (
+      <div key={index}>
+        <img src={url} alt={altText} />
+      </div>
+    ))}
+  </GalleryWrap>
+</Wrapper>
+)}
--- src/templates/project.js
+++ src/templates/project.js
@@ -1,85 +1,79 @@
 import React from 'react'
 import Layout from '../components/layout'
-import { graphql } from 'gatsby'
+import Gallery from '../components/gallery'
+import { withPrefix, graphql } from 'gatsby'
 import Helmet from 'react-helmet'
 import styled from 'react-emotion'
 
+import background from '../images/diag-lines.svg'
+
+const Article = styled.article`
+  margin: 0 10.41%;
+`
+
 const MainImgWrap = styled.div`
-    img {
-        display: block;
-        border: 2px ${props => props.theme.bicblue} solid;
-        width: calc(100%/6 * 4);
-        margin: ${props => props.theme.txtsm} auto 0;
+  width: 100%;
+  border: solid 2px #e5e5e5;
+  height: 74vh;
+  padding: 5vh;
+  background: url(${background});
+  @media (max-width: 420px) {
+         background: none;
+         border: none;
+         padding: 0;
+         height: auto;
+  }
+  img {
+    display: block;
+    max-height: 100%;
+    margin: 0 auto;
+    object-fit: contain;
+    @media (max-width: 420px) {
+            width: 100%;
     }
+  }
 `
 
 const TitleWrap = styled.div`
-    display: flex;
-    flex-wrap: nowrap;
-    align-items: flex-end;
-    width: 100%;
-    justify-content: center;
-        div {
-            width: calc(100%/6 * 2);
-            padding-left: ${props => props.theme.txtsm};
-        }
+  display: flex;
+  flex-wrap: wrap;
+  align-items: flex-end;
+  justify-content: flex-start;
+  width: 100%;
+  margin: 0 23px 23px 23px;
+  @media (max-width: 420px) {
+      margin: 0 0 23px 0;
+  }
 `
 
 const ProjectTitle = styled.h1`
-    font-weight: 200;
-    font-size: ${props => props.theme.txtlrg};
-    margin-top: ${props => props.theme.txtmd};
+  font-weight: 600;
+  font-size: ${props => props.theme.txtlrg};
 `
 
 const Work = styled.h2`
-    font-weight: 200;
-    font-size: ${props => props.theme.txtmd};
-    text-align: right;
+  font-weight: 400;
+  font-size: ${props => props.theme.txtlrg};
+  span {
+    font-style: italic;
+  }
 `
 
 const DescriptionWrap = styled.div`
-display: flex;
-flex-wrap: nowrap;
-align-items: flex-end;
-width: 100%;
-justify-content: center;
-    div {
-        width: calc(100%/6 * 4);
-        padding-right: calc(100%/6 * 1);
-        margin-top: 1rem;
-    }
-    p {
-        font-weight: 300;
-        line-height: 156%;
-        margin-top: .75rem;
-    }
-`
-
-const Wrapper = styled.section`
-    display: flex;
-    width: 100%;
-    justify-content: center;
-`
-
-const GalleryWrap = styled.div`
-    display: flex;
-    flex-wrap: wrap;
-    align-items: flex-end;
-    width: calc(100%/6 * 4);
-        div {
-            width: calc(100%/2);
-            margin: ${props => props.theme.txtsm} 0;
-            :nth-of-type(odd) {
-                padding-right: ${props => props.theme.txtsm};
-            }
-            :nth-of-type(even) {
-                padding-left: ${props => props.theme.txtsm};
-            }
-        }
-        img {
-            width: 100%;
-            border: 2px ${props => props.theme.bicblue} solid;
-        }
+  display: flex;
+  flex-wrap: nowrap;
+  align-items: flex-end;
+  width: 100%;
+  justify-content: center;
+  margin-bottom: 114px;
+  div {
+    width: 750px;
+    margin: 23px auto 0;
+  }
+  p {
+    font-weight: 400;
+    line-height: 156%;
+  }
 `
 
 const showdown = require('showdown')
@@ -95,38 +89,23 @@ export default ({ data }) => {
   return (
     <Layout>
       <Helmet>
-        <title>{'Patrick O\'Connell - ' + data.project.title}</title>
-        <style type="text/css">{`
-            body {
-                background: linear-gradient(to bottom, rgba(152,204,132,1) 0%, rgba(152,204,132,0.36) 64%, rgba(221,151,91,0.16) 84%, rgba(221,151,91,0) 100%) no-repeat;
-                background-size: 100% 100vh;
-            }`}</style>
-    </Helmet>
-    <article>
-    <MainImgWrap>
-        <img src={data.project.heroImage.url} />
-    </MainImgWrap>
-    <TitleWrap>
-    <div>
-      <ProjectTitle>{data.project.title}</ProjectTitle>
-    </div>
-    <div>
-       <Work>{data.project.workType}</Work>
-     </div>
-    </TitleWrap>
-    <DescriptionWrap>
-        <div dangerouslySetInnerHTML={createMarkup()} />
-     </DescriptionWrap>
-     <Wrapper>
-      <GalleryWrap>
-        {data.project.galleryImage.map(({ url, altText }, index) => (
-          <div key={index}>
-            <img src={url} alt={altText} />
-          </div>
-        ))}
-      </GalleryWrap>
-      </Wrapper>
-      </article>
+        <title>{"Patrick O'Connell - " + data.project.title}</title>
+      </Helmet>
+      <Article>
+        <TitleWrap>
+          <ProjectTitle>{data.project.title}&nbsp;</ProjectTitle>
+          <Work>
+            ( <span>{data.project.workType}</span> )
+          </Work>
+        </TitleWrap>
+        <MainImgWrap>
+          <img src={data.project.heroImage.url} />
+        </MainImgWrap>
+        <Gallery galleryImage={data.project.galleryImage} heroThumb={data.project.heroImage.url} />
+        <DescriptionWrap>
+          <div dangerouslySetInnerHTML={createMarkup()} />
+        </DescriptionWrap>
+      </Article>
     </Layout>
   )
 }
@@ -141,8 +120,8 @@ export const ProjectPageQuery = graphql`
       description
       workType
       heroImage {
-          url
-          altText
+        url
+        altText
       }
       galleryImage {
         url
