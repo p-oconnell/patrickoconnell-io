6f8711f501732e010fcd05f8e7516564e7ea3ec8
Patrick O'Connell <poconnell@Patricks-MacBook-Pro.local>
2020-10-01 21:26:14 -0700

    Add lazy loading and placeholder images to featured-img

--- src/components/fearured-img.js
+++ src/components/fearured-img.js
@@ -2,40 +2,56 @@ import React from 'react'
 import { Link } from 'gatsby'
 import styled from 'react-emotion'
 
-const HeroLnk = styled(Link)`
+import LazyLoad from 'react-lazy-load'
+
+import ImageLoad from './imageloader'
+
+const FeaturedLnk = styled(Link)`
   color: inherit;
   text-decoration: none;
 `
 
-const HeroPicWrap = styled.div`
-    width: calc(100%/3*2);
-    height: 450px;
-    @media (max-width: 420px) {
-        height: 45vh;
-        width: 100%;
-    }
+const FeaturedPicWrap = styled.div`
+  border: ${props => props.theme.border};
+  width: calc(100% / 3 * 2);
+  height: 450px;
+  @media (max-width: 420px) {
+    height: 45vh;
+    width: 100%;
+    border: ${props => props.theme.borderMobile};
+  }
 `
 
-const HeroPic = styled.picture`
+const FeaturedPic = styled.picture`
   img {
-    border: ${props => props.theme.border};
-    width:100%;
-    height: 450px;
+    width: 100%;
+    height: calc(450px - 24px);
     object-fit: cover;
     @media (max-width: 420px) {
-        height: 45vh;
-        border: ${props => props.theme.borderMobile};
+      height: calc(45vh - 14px);
     }
   }
 `
 
-export default function FeaturedTitle(props) {
-    return (
-<HeroPicWrap>
-<HeroLnk to={'/' + props.slug}>
-  <HeroPic>
-    <img src={props.src} alt={props.alt} />
-  </HeroPic>
-</HeroLnk>
-</HeroPicWrap>
-    )}
+export default function FeaturedImg(props) {
+  return (
+    <FeaturedPicWrap>
+      <FeaturedLnk to={'/' + props.slug}>
+        <FeaturedPic>
+          <LazyLoad debounce={false} offsetVertical={500}>
+            <ImageLoad
+              src={
+                'https://media.graphcms.com/auto_image/compress/' + props.src
+              }
+              placeholder={
+                'https://media.graphcms.com/resize=width:40/blur=amount:2/' +
+                props.handle
+              }
+              alt={props.alt}
+            />
+          </LazyLoad>
+        </FeaturedPic>
+      </FeaturedLnk>
+    </FeaturedPicWrap>
+  )
+}
