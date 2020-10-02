5bf371efabdeec5a298fdbc82677ca6675cbdcd4
Patrick O'Connell <poconnell@Patricks-MacBook-Pro.local>
2020-10-01 21:28:59 -0700

    Update component names

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,14 +1,14 @@
 import React from 'react'
 
 import Layout from '../components/layout'
-import Hero from '../components/hero'
+import Featured from '../components/featured'
 import Grid from '../components/grid'
 
 export default ({ data }) => {
   return (
     <Layout>
       <div>
-        <Hero />
+        <Featured />
         <Grid />
       </div>
     </Layout>
