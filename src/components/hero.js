0dc67d2a9a1eb287c3f9ade8d4db22aae160b11e
Patrick O'Connell <poconnell@Patricks-MacBook-Pro.local>
2020-10-22 16:00:40 -0700

    Added Hero component

---
+++ src/components/hero.js
@@ -0,0 +1,53 @@
+import React from 'react'
+import styled from 'react-emotion'
+
+import background from '../images/diag-lines.svg'
+
+const MainImgWrap = styled.div`
+  width: 100%;
+  border: solid 2px #e5e5e5;
+  height: 68vh;
+  padding: 1.5vh;
+  background: url(${background});
+  overflow: hidden;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  position: relative;
+  cursor: pointer;
+  @media (max-width: 420px) {
+    background: none;
+    border: none;
+    padding: 0;
+    height: auto;
+  }
+  img {
+    display: block;
+    max-height: 100%;
+    margin: 0 auto;
+    object-fit: contain;
+    @media (max-width: 420px) {
+      max-width: 100%;
+      max-height: 60vh;
+    }
+  }
+`
+
+const Wrapper = styled.section`
+  display: flex;
+  width: 100%;
+  justify-content: center;
+`
+
+export default function Hero(props) {
+  return (
+    <div>
+      <MainImgWrap className="lightbox">
+        <img
+          src={'https://media.graphcms.com/auto_image/compress/' + props.handle}
+          alt={props.altText}
+        />
+      </MainImgWrap>
+    </div>
+  )
+}
