7d5e961f52c522983d7e5a3c17f02d64f94fec70
Patrick O'Connell <poconnell@Patricks-MacBook-Pro.local>
2020-10-22 16:05:00 -0700

    Lightbox and Sass added to project

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,6 +6,7 @@ module.exports = {
     'gatsby-plugin-react-helmet',
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
+    `gatsby-plugin-sass`,
     {
       resolve: `gatsby-source-graphcms`,
       options: {
