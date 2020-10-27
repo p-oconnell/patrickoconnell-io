2460fbeda3ab685334e9b7f4a8dfba947c2f34e5
Patrick O'Connell <poconnell@Patricks-MacBook-Pro.local>
2020-10-26 23:37:48 -0700

    Update gatsby-config

--- gatsby-config.js
+++ gatsby-config.js
@@ -25,13 +25,13 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: 'gatsby-starter-default',
-        short_name: 'starter',
+        name: 'patrickoconnell-io',
+        short_name: 'po-io',
         start_url: '/',
         background_color: '#663399',
         theme_color: '#663399',
         display: 'minimal-ui',
-        icon: 'src/images/gatsby-icon.png', // This path is relative to the root of the site.
+        icon: 'src/images/po-io-icon.png', // This path is relative to the root of the site.
       },
     },
     'gatsby-plugin-offline',
